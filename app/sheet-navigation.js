832d7606f23ba484af40927cdb9854e837e77cd8
charlesarmitage <code@cjrarmitage.co.uk>
2015-04-23 10:05:22 +0100

    Fix back button events for hiding menu

--- app/sheet-navigation.js
+++ app/sheet-navigation.js
@@ -7,6 +7,11 @@
 	$('#menu-btn').on('click', function(){
 	});
 
+	var isNavOpen = false;
+	$('.navbar-collapse').on('shown.bs.collapse', function () {
+	  isNavOpen = true;
+	});
+
 	$(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
 	   var hash = e.target.hash;
 	   if(hash == '#Statistics'){
@@ -16,21 +21,25 @@
 		}
 	});
 
-    $(document).on('deviceready', function(){
-    	document.addEventListener("backbutton", onBackButton, false);
-    	document.addEventListener("menubutton", onMenuKeyDown, false);
-    });
-
 	$(document).ready(function(){
 		$('#statistics-summary').hide();
 	});
 
+	document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady(){
+    	document.addEventListener("backbutton", onBackButton, false);
+    	document.addEventListener("menubutton", onMenuKeyDown, false);
+    };
+
 	function onBackButton(){
-		$(".navbar-toggle").click();
+		if(isNavOpen){ // Avoid first back button press opening the navbar-collapse
+			$(".navbar-collapse").collapse('hide');
+    		isNavOpen = false;
+		}
 	}
 
 	function onMenuKeyDown(){
 		$(".navbar-toggle").click();
 	}
-
 })();
